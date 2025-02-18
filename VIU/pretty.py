cfe8cf68d9b39006eb0caaabc04abbdcb5731897
Mo Zhou <cdluminate@gmail.com>
2025-02-18 11:08:17 -0500

    viu: pretty print

--- VIU/pretty.py
+++ VIU/pretty.py
@@ -12,6 +12,16 @@ viu14.wse.jhu.edu | CHANGED | rc=0 | (stdout) index, name, utilization.gpu [%], 
 '''
 import sys
 import re
+import rich
+console = rich.get_console()
+
+RESET = '\033[0m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BG_RED = '\033[41m'
+BG_GREEN = '\033[42m'
+BG_YELLOW = '\033[43m'
 
 for line in sys.stdin:
     line = line.strip()
@@ -23,6 +33,28 @@ for line in sys.stdin:
     elif re.match(r'^\d+,', line):
         m = re.match(r'(\d+), (.*?), (.*?), (.*?), (.*?)$', line)
         idx, name, util, used, total = m.groups()
-        print(f'| {idx.ljust(2)} | {name.ljust(28)} | {util.rjust(6)} | {used.rjust(10)} | {total.rjust(10)} |', flush=True)
+        idx = idx.ljust(2)
+        name = name.ljust(28)
+        util = util.ljust(6)
+        used = used.rjust(10)
+        total = total.rjust(10)
+        mem_ratio = float(used.split()[0]) / float(total.split()[0])
+        utility = int(util.split()[0])
+        if mem_ratio > 0.5 or utility > 80:
+            name = BG_RED + name + RESET
+            used = RED + used + RESET
+        elif mem_ratio > 0.2 or utility > 50:
+            name = BG_YELLOW + name + RESET
+            used = YELLOW + used + RESET
+        else:
+            name = BG_GREEN + name + RESET
+            used = GREEN + used + RESET
+        if utility > 50:
+            util = RED + util + RESET
+        elif utility > 20:
+            util = YELLOW + util + RESET
+        else:
+            util = GREEN + util + RESET
+        print(f'| {idx} | {name} | {util} | {used} | {total} |', flush=True)
     else:
         print('no match. seems to be a bug', flush=True)
